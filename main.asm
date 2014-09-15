dc94fe764afbcd61156347f94203c5479432f3fc
Brian Knoblauch <briank@glmotorsports.net>
2014-09-14 20:20:46 -0400

    Improve make file error handling and generate about box.

--- main.asm
+++ main.asm
@@ -1,13 +1,26 @@
 .686
-.model flat, stdcall 
+.model flat, stdcall
+option casemap:none
 
 include \masm32\include\kernel32.inc
+include \masm32\include\user32.inc
+include \masm32\include\windows.inc 
 includelib \masm32\lib\kernel32.lib 
+includelib \masm32\lib\user32.lib
 
 .data 
+    about_caption   db  "About ProjectCQB...",0
+    about_message   db  "In a world of conflict, few natural resources, and little energy, what's old is new again...",13,13,10,
+                        "Version 1.0 Alpha",0
     
 .code
 start:
+    call    about
     invoke  ExitProcess, 0
+
+about   PROC
+    invoke  MessageBox, NULL, offset about_message, offset about_caption, MB_OK
+    ret
+about   ENDP
     
 end start
--- make.bat
+++ make.bat
@@ -1,7 +1,17 @@
 @echo off
 echo ...Assembling...
 c:\masm32\bin\ml.exe /c /coff /Cp main.asm
+if ERRORLEVEL 1 GOTO assemblyerror
 echo ...Linking...
-c:\masm32\bin\link.exe /SUBSYSTEM:WINDOWS /LIBPATH:c:\masm32\lib main.obj /OUT:dist\main.exe
+c:\masm32\bin\link.exe /SUBSYSTEM:CONSOLE /LIBPATH:c:\masm32\lib main.obj /OUT:dist\main.exe
+if ERRORLEVEL 1 GOTO linkererror
 echo ...Cleaning...
 del *.obj
+GOTO End
+:assemblyerror
+echo ...Assembly error...
+GOTO End
+:linkererror
+echo ...Linker error...
+GOTO End
+:End
